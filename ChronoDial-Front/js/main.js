09a1a14bcefd03378d13f9250aa80a417f3c942f
Nihattaghizade <nihattagizade2003@gmail.com>
2024-05-16 18:47:49 +0400

    commit seventh

--- ChronoDial-Front/js/main.js
+++ ChronoDial-Front/js/main.js
@@ -1,74 +1,106 @@
-"use strict";
+// "use strict";
 
-const menuIcon = document.querySelectorAll(".menubutton"); 
-const menuContainer = document.querySelector(".menucontainer");
-const menuCloseBtn = document.querySelectorAll(".menuCloseButton");
+// const menuIcon = document.querySelectorAll(".menubutton"); 
+// const menuContainer = document.querySelector(".menucontainer");
+// const menuCloseBtn = document.querySelectorAll(".menuCloseButton");
 
-const searchBtn = document.querySelectorAll(".searchButton");
-const searchSection = document.querySelector(".searchsection");
-const searchCloseBtn = document.querySelectorAll(".searchCloseButton");
+// const searchBtn = document.querySelectorAll(".searchButton");
+// const searchSection = document.querySelector(".searchsection");
+// const searchCloseBtn = document.querySelectorAll(".searchCloseButton");
 
-const menuConPageBoxInner = document.querySelectorAll(".pagebox");
-const menuList = document.querySelector(".menulist");
-const pageList = document.querySelector(".pagelist");
+// const menuConPageBoxInner = document.querySelectorAll(".pagebox");
+// const menuList = document.querySelector(".menulist");
+// const pageList = document.querySelector(".pagelist");
 
-const basketOpenBtn = document.querySelectorAll(".cart-button");
-const basketContainer = document.querySelector(".basketcontainer");
-const basketCloseBtn = document.querySelectorAll(".basketCloseButton");
+// const basketOpenBtn = document.querySelectorAll(".cart-button");
+// const basketContainer = document.querySelector(".basketcontainer");
+// const basketCloseBtn = document.querySelectorAll(".basketCloseButton");
 
-menuIcon.forEach( (e) => {
-    e.addEventListener("click", () => {
-        menuContainer.classList.remove("hidden");
-    });
-});
+// menuIcon.forEach( (e) => {
+//     e.addEventListener("click", () => {
+//         menuContainer.classList.remove("hidden");
+//     });
+// });
 
-menuCloseBtn.forEach( (e) => {
-    e.addEventListener("click", () => {
-        menuContainer.classList.add("hidden");
-    });
-});
+// menuCloseBtn.forEach( (e) => {
+//     e.addEventListener("click", () => {
+//         menuContainer.classList.add("hidden");
+//     });
+// });
 
-basketOpenBtn.forEach( (e) => {
-  e.addEventListener("click", () => {
-      basketContainer.classList.remove("hidden");
-  });
-});
+// basketOpenBtn.forEach( (e) => {
+//   e.addEventListener("click", () => {
+//       basketContainer.classList.remove("hidden");
+//   });
+// });
 
-basketCloseBtn.forEach( (e) => {
-  e.addEventListener("click", () => {
-      basketContainer.classList.add("hidden");
-  });
-});
+// basketCloseBtn.forEach( (e) => {
+//   e.addEventListener("click", () => {
+//       basketContainer.classList.add("hidden");
+//   });
+// });
 
-searchBtn.forEach( (e) => {
-    e.addEventListener("click", () => {
-        searchSection.classList.remove("hidden");
-    });
-});
+// searchBtn.forEach( (e) => {
+//     e.addEventListener("click", () => {
+//         searchSection.classList.remove("hidden");
+//     });
+// });
 
-searchCloseBtn.forEach( (e) => {
-    e.addEventListener("click", () => {
-        searchSection.classList.add("hidden");
-    });
-});
+// searchCloseBtn.forEach( (e) => {
+//     e.addEventListener("click", () => {
+//         searchSection.classList.add("hidden");
+//     });
+// });
 
-menuConPageBoxInner.forEach( (e) => {
-    e.addEventListener("click", () => {
-        menuList.classList.toggle("hidden");
-        pageList.classList.toggle("hidden");
-    });
-});
+// menuConPageBoxInner.forEach( (e) => {
+//     e.addEventListener("click", () => {
+//         menuList.classList.toggle("hidden");
+//         pageList.classList.toggle("hidden");
+//     });
+// });
 
 
 
-// const decreaseBtn = document.querySelectorAll('.decreasebtn');
-// const increaseBtn = document.querySelectorAll('.increasebtn');
-// const counter = document.getElementById('counter');
+// const decreaseBtn = document.querySelector('.decreasebtn');
+// const increaseBtn = document.querySelector('.increasebtn');
+// const counterInput = document.getElementById('counter');
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const decreaseBtn = document.querySelectorAll('.decreasebtn');
+//     const increaseBtn = document.querySelectorAll('.increasebtn');
+//     const counterInput = document.querySelectorAll('.counter');
+
+//     increaseBtn.addEventListener('click', () => {
+//         counterInput.value = parseInt(counterInput.value) + 1;
+//     });
+
+//     decreaseBtn.addEventListener('click', () => {
+//         let currentValue = parseInt(counterInput.value);
+//         if (currentValue > 0) {
+//             counterInput.value = currentValue - 1;
+//         }
+//     });
+// });
+
+
+// increaseBtn.addEventListener("click", () => {
+    
+//         counterInput.value++;
+    
+// });
+
+// decreaseBtn.addEventListener("click", () => { 
+    
+
+//     if(counterInput >= 2){
+//         counterInput.value--;
+//     }
+// });
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    const decreaseBtn = document.querySelectorAll('.decreasebtn');
-    const increaseBtn = document.querySelectorAll('.increasebtn');
-    const counterInput = document.querySelectorAll('.counter');
+    const decreaseBtn = document.querySelector('.decreasebtn');
+    const increaseBtn = document.querySelector('.increasebtn');
+    const counterInput = document.getElementById('counter');
 
     increaseBtn.addEventListener('click', () => {
         counterInput.value = parseInt(counterInput.value) + 1;
@@ -76,7 +108,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     decreaseBtn.addEventListener('click', () => {
         let currentValue = parseInt(counterInput.value);
-        if (currentValue > 0) {
+        if (currentValue > 1) {
             counterInput.value = currentValue - 1;
         }
     });
